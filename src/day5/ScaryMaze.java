fa1c7e588d502450b438d7aacf69722d7ef5a2e3
Derpy Steve <league@WTS14.attlocal.net>
2016-08-05 12:02:29 -0700

    five
    maze

--- src/day5/ScaryMaze.java
+++ src/day5/ScaryMaze.java
@@ -2,7 +2,6 @@ package day5;
 
 import java.applet.AudioClip;
 import java.awt.Dimension;
-
 import java.awt.Graphics;
 import java.awt.Robot;
 import java.awt.event.MouseEvent;
@@ -12,24 +11,28 @@ import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
-	
+
 	BufferedImage maze;
 	final int frameWidth = 600;
 	final int frameHeight = 400;
+	JFrame frame;
 
 	ScaryMaze() throws Exception {
-		//1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
+		// 1. Use this online tool to make a maze image and drop it into your day5 package: http://pixlr.com/editor/
 		maze = ImageIO.read(getClass().getResource("maze.png"));
-		//2. set the mouse pointer to the start of your maze using:
-		//new Robot().mouseMove(xPosition, yPosition)
-		
-		//3. add a mouse motion listener using:
-		//addMouseMotionListener(this)
-		
+		// 2. set the mouse pointer to the start of your maze using:
+		// new Robot().mouseMove(xPosition, yPosition)
+		Robot you = new Robot();
+		you.mouseMove(10, 60);
+
+		// 3. add a mouse motion listener using:
+		addMouseMotionListener(this);
+
 	}
 
 	@Override
@@ -37,28 +40,35 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		int mouseX = e.getX();
 		int mouseY = e.getY();
 		int mouseColor = maze.getRGB(mouseX, mouseY);
-		//4. print the mouseColor variable to see what color the mouse is touching
+		// 4. print the mouseColor variable to see what color the mouse is touching
+		System.out.println(mouseColor);
+		// 5. make a variable to hold the background color.
+		int backColor = -16777216;
+		// 6. if the mouse falls off the path (if it is on the background)
+		if (mouseColor == backColor) {
+			scare();
+		}
+		// call the scare method
 
-		//5. make a variable to hold the background color. 
+		// 10. if the mouse is on the end color
+		if (mouseColor == -16744781) {
+			JOptionPane.showMessageDialog(null, "You won");
+			// pop up a message to tell them they won
 
-		//6. if the mouse falls off the path (if it is on the background)
-		
-				// call the scare method
-		
-		//10. if the mouse is on the end color
-				
-				// pop up a message to tell them they won
-		
+		}
 	}
 
 	private void scare() {
 		System.out.println("BOO!");
-		//7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on freesound.org. Log in as leagueofamazing/code4life.
-		//AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav"));
-		
-		//8. play the scary sound. Hint: type "sound" and then a period.		
-		
-		//9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
+		// 7. find a scary sound and put it in the day5 package where you put your maze picture. You can find a sound on
+		// freesound.org. Log in as leagueofamazing/code4life.
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource("scream.wav.wav"));
+
+		// 8. play the scary sound. Hint: type "sound" and then a period.
+		sound.play();
+		// 9. drop an image into your day5 package, and use the showScaryImage method to scare your victim!
+
+		showScaryImage("scary guy.png");
 
 	}
 
@@ -68,6 +78,8 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 		} catch (Exception e) {
 			System.err.println("Couldn't find this image: " + imageName);
 		}
+		frame.setSize(maze.getWidth(), maze.getHeight());
+
 		repaint();
 	}
 
@@ -77,12 +89,12 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 
 	@Override
 	public void run() {
-		JFrame frame = new JFrame("June's Scary Maze");
+		frame = new JFrame("June's Scary Maze");
 		frame.add(this);
 		setPreferredSize(new Dimension(frameWidth, frameHeight));
 		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setResizable(false);
+		// frame.setResizable(false);
 		frame.setVisible(true);
 	}
 
@@ -92,9 +104,7 @@ public class ScaryMaze extends JPanel implements Runnable, MouseMotionListener {
 	}
 
 	@Override
-	public void mouseDragged(MouseEvent e) {}
+	public void mouseDragged(MouseEvent e) {
+	}
 
 }
-
-
-
